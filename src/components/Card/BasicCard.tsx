90247f85122bf43cbff39ef29f3f482cdb5ac21d
Salimov Sergei <sergesalimov@yandex.ru>
2024-06-21 01:15:32 +0300

    Display logo on cards

--- src/components/Card/BasicCard.tsx
+++ src/components/Card/BasicCard.tsx
@@ -1,14 +1,22 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
 
 import Pass from '../../interfaces/IPass';
+import PassBundle from '../../interfaces/IPassBundle';
 
-function BasicCard({ pass }: { pass: Pass}) {
+function BasicCard({ passBundle }: { passBundle: PassBundle}) {
+    const pass = passBundle.objects.pass
+    
+    let logoSrc: string = ''
+    if (passBundle.files.logo) {
+        logoSrc = URL.createObjectURL(passBundle.files.logo)
+    }
 
     return (
         <Card style={{ width: '20rem', backgroundColor: pass.backgroundColor, color: pass.foregroundColor}}>
             <Card.Body>
-                <Card.Title>{pass.description}</Card.Title>
+                {passBundle.files.logo ? <Image src={logoSrc} style={{maxWidth: '50%'}}/> : null}
                 <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
