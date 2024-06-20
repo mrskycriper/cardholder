09d4e515d7b2ab6fc1aa9ed368bee9fed4689056
Salimov Sergei <sergesalimov@yandex.ru>
2024-06-20 22:21:53 +0300

    Add mockup of generic card component

--- src/components/Card/BasicCard.tsx
+++ src/components/Card/BasicCard.tsx
@@ -1,16 +1,18 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function BasicCard() {
+import Pass from '../../interfaces/IPass';
+
+function BasicCard({ pass }: { pass: Pass}) {
+
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '20rem', backgroundColor: pass.backgroundColor, color: pass.foregroundColor}}>
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{pass.description}</Card.Title>
                 <Card.Text>
                     Some quick example text to build on the card title and make up the
                     bulk of the card's content.
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
             </Card.Body>
         </Card>
     );
