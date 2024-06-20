941a2812a992c81ccff0a022942503e35bba8a9e
Salimov Sergei <sergesalimov@yandex.ru>
2024-06-21 01:15:03 +0300

    Load logo file from OPFS

--- src/components/CardStack/worker.ts
+++ src/components/CardStack/worker.ts
@@ -3,7 +3,6 @@
 import PassBundle from '../../interfaces/IPassBundle';
 
 self.onmessage = async (e: MessageEvent<String>) => {
-    console.log(`Worker: got ${e.data}`)
     const opfsRoot = await navigator.storage.getDirectory();
     const defaultDirectory = await opfsRoot.getDirectoryHandle("default", {
         create: true,
@@ -12,11 +11,40 @@ self.onmessage = async (e: MessageEvent<String>) => {
     const files: PassBundle[] = [];
 
     for await (const [directoryName, directoryHandle] of defaultDirectory.entries()) {
+        let passBundle: PassBundle
+
         // @ts-ignore
         const passFileHandle: FileSystemFileHandle = await directoryHandle.getFileHandle('pass.json');
         const passFile: File = await passFileHandle.getFile()
         const passObject = JSON.parse(await passFile.text())
-        files.push({ name: directoryName, objects: {pass: passObject} })
+
+        passBundle = { id: directoryName, objects: {pass: passObject}, files: {} }
+
+        let logoFileHandle: FileSystemFileHandle | undefined = undefined;
+        try {
+            // @ts-ignore
+            logoFileHandle = await directoryHandle.getFileHandle('logo.png');
+        } catch (e) {
+            // not found, silent skip
+        }
+        try {
+            // @ts-ignore
+            logoFileHandle = await directoryHandle.getFileHandle('logo@2x.png');
+        } catch (e) {
+            // not found, silent skip
+        }
+        try {
+            // @ts-ignore
+            logoFileHandle = await directoryHandle.getFileHandle('logo@3x.png');
+        } catch (e) {
+            // not found, silent skip
+        }
+        if (logoFileHandle !== undefined) {
+            const logoFile: File = await logoFileHandle.getFile()
+            passBundle.files.logo = logoFile
+        }
+
+        files.push(passBundle)
     }
 
     self.postMessage(files);
