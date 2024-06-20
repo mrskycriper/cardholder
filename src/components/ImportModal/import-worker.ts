6925219634e2859413ed630133d6685a4a3da155
Salimov Sergei <sergesalimov@yandex.ru>
2024-06-20 14:50:41 +0300

    Use JSZip to unzip passes before saving

--- src/components/ImportModal/import-worker.ts
+++ src/components/ImportModal/import-worker.ts
@@ -1,22 +1,35 @@
 /* eslint-disable no-restricted-globals */
 
+import JSZip from "jszip";
+
 self.onmessage = async (e: MessageEvent<File>) => {
     console.log(`Worker: got ${e.data.name}`)
     const opfsRoot = await navigator.storage.getDirectory();
     const defaultDirectory = await opfsRoot.getDirectoryHandle("default", {
         create: true,
     });
-    const fileHandle = await defaultDirectory.getFileHandle(e.data.name, {
+    const unzippedDirectory = await defaultDirectory.getDirectoryHandle(e.data.name, {
         create: true,
     });
-    // @ts-ignore
-    // Thinks property 'createSyncAccessHandle' does not exist on type 'FileSystemFileHandle' for some reason
-    const accessHandle = await fileHandle.createSyncAccessHandle();
-    const fileData = await e.data.arrayBuffer()
-    accessHandle.write(fileData)
-    accessHandle.close()
+
+    JSZip.loadAsync(await e.data.arrayBuffer())
+        .then(function (zip) {
+            zip.forEach(async function (relativePath, zipEntry) {
+                const fileHandle = await unzippedDirectory.getFileHandle(zipEntry.name, {
+                    create: true,
+                });
+                // @ts-ignore
+                // Thinks property 'createSyncAccessHandle' does not exist on type 'FileSystemFileHandle' for some reason
+                const accessHandle = await fileHandle.createSyncAccessHandle();
+                const fileData = await zipEntry.async('arraybuffer');
+                accessHandle.write(fileData)
+                accessHandle.close()
+            });
+        }, function (e) {
+            console.log(`Worker: got error ${e}`)
+        });
 
     self.postMessage(`Saved ${e.data.name}`);
 };
-  
-export {};
+
+export { };
