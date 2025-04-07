2423d412bb7ce6d838a4c887fa0ab3f0bfab3682
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2025-04-07 21:59:02 +0300

    Add opfs constants

--- src/constants/files.ts
+++ src/constants/files.ts
@@ -1,1 +1,10 @@
 export const DEFAULT_FOLDER = "default";
+
+export const IMAGE_FILES: Record<string, (scale?: string) => string> = {
+  icon: (scale?: string): string => `icon${scale}.png`,
+  logo: (scale?: string): string => `logo${scale}.png`,
+  strip: (scale?: string): string => `strip${scale}.png`,
+  background: (scale?: string): string => `background${scale}.png`,
+  thumbnail: (scale?: string): string => `thumbnail${scale}.png`,
+  footer: (scale?: string): string => `footer${scale}.png`,
+};
