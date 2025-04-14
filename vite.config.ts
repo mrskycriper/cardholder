c42c7800c313fb9fcdedb00912a37a0084ddf165
mrskycriper <32013287+mrskycriper@users.noreply.github.com>
2025-04-14 21:32:13 +0300

    Update vitepwa config

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,27 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import basicSsl from "@vitejs/plugin-basic-ssl"
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            injectRegister: 'script',
-
-            pwaAssets: {
-                disabled: true,
-                config: false,
-            },
-
-            manifest: false,
-
-            workbox: {
-                globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-                cleanupOutdatedCaches: true,
-                clientsClaim: true,
-            },
-
-            devOptions: {
-                enabled: false,
-                navigateFallback: 'index.html',
-                suppressWarnings: true,
-                type: 'module',
-            },
-        }),
-        basicSsl()
-    ],
-    server: {
-        // @ts-ignore
-        https: true,
-        host: true,
-    },
-    base : '/cardholder/'
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+      },
+      manifest: false,
+    }),
+    basicSsl(),
+  ],
+  server: {
+    // @ts-expect-error SmthSmthSmth
+    https: true,
+    host: true,
+  },
+  base: "/cardholder/",
+});
